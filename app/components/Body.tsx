d14d99ad6cd00cce03fc7ad167929f937bb789e9
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-07-06 03:59:16 +0600

    minor update

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -2,8 +2,9 @@ import Clock from "./Clock";
 
 const Body = () => {
     return (
-        <div className="flex justify-center items-center flex-col mb-12  " > 
-            <div className="bg-slate-400 dark:bg-cyan-900 bg-opacity-15 dark:bg-opacity-35 rounded-xl flex">
+        <div className="flex justify-center items-center flex-col mb-12" > 
+            <p className="text-5xl pb-4">بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</p>
+            <div className="bg-slate-400 dark:bg-cyan-900 bg-opacity-15 dark:bg-opacity-45 rounded-xl flex">
                 <Clock/>
             </div>
         </div>
